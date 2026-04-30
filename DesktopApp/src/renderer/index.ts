1eb044bdb19cd95fe5cde065d977923c66fa38ba
M B A R K <mbark1236@gmail.com>
2026-04-30 18:01:02 +0100

    ui: Upgrade the UI Design to be Neumorphic

--- DesktopApp/src/renderer/index.ts
+++ DesktopApp/src/renderer/index.ts
@@ -149,21 +149,15 @@ function buildShell(deviceName: string, platform: NodeJS.Platform): string {
 
         <div class="sidebar__nav" role="menubar">
           <a href="#/home" class="sidebar__nav-item sidebar__nav-item--active" data-route="/home" role="menuitem">
-            <svg class="sidebar__nav-item-icon" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>
-            </svg>
+            <i class="fa-solid fa-house sidebar__nav-item-icon" aria-hidden="true"></i>
             <span class="sidebar__nav-item-label">Home</span>
           </a>
           <a href="#/transfers" class="sidebar__nav-item" data-route="/transfers" role="menuitem">
-            <svg class="sidebar__nav-item-icon" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path d="M8 5a1 1 0 100 2h5.586l-1.293 1.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L13.586 5H8zM12 15a1 1 0 100-2H6.414l1.293-1.293a1 1 0 10-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L6.414 15H12z"/>
-            </svg>
+            <i class="fa-solid fa-arrow-right-arrow-left sidebar__nav-item-icon" aria-hidden="true"></i>
             <span class="sidebar__nav-item-label">Transfers</span>
           </a>
           <a href="#/settings" class="sidebar__nav-item" data-route="/settings" role="menuitem">
-            <svg class="sidebar__nav-item-icon" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
-            </svg>
+            <i class="fa-solid fa-gear sidebar__nav-item-icon" aria-hidden="true"></i>
             <span class="sidebar__nav-item-label">Settings</span>
           </a>
         </div>
