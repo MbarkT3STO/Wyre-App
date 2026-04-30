ba7ed5c0f33fe27f22dadd86fd6b8f730cf60eac
M B A R K <mbark1236@gmail.com>
2026-04-30 18:21:13 +0100

    ui: Update the Settings Tab Icons to be Neumorphic

--- DesktopApp/src/renderer/views/SettingsView.ts
+++ DesktopApp/src/renderer/views/SettingsView.ts
@@ -10,16 +10,16 @@ import type { AppSettings } from '../../shared/models/AppSettings';
 import type { ToastContainer } from '../components/ToastContainer';
 import { validateDeviceName, validatePort, validateTimeout } from '../../shared/utils/validators';
 
-// SVG icons for each section
+// Font Awesome icons for each section — consistent with the rest of the UI
 const ICONS = {
-  identity: `<svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"/></svg>`,
-  transfer: `<svg viewBox="0 0 20 20" fill="currentColor"><path d="M8 5a1 1 0 100 2h5.586l-1.293 1.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L13.586 5H8zM12 15a1 1 0 100-2H6.414l1.293-1.293a1 1 0 10-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L6.414 15H12z"/></svg>`,
-  incoming: `<svg viewBox="0 0 20 20" fill="currentColor"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zm0 16a3 3 0 01-3-3h6a3 3 0 01-3 3z"/></svg>`,
-  appearance: `<svg viewBox="0 0 20 20" fill="currentColor"><path d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 110-2 1 1 0 010 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z"/></svg>`,
-  notifications: `<svg viewBox="0 0 20 20" fill="currentColor"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zm0 16a3 3 0 01-3-3h6a3 3 0 01-3 3z"/></svg>`,
-  data: `<svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/></svg>`,
-  folder: `<svg viewBox="0 0 20 20" fill="currentColor"><path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"/></svg>`,
-  port: `<svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"/></svg>`,
+  identity:      `<i class="fa-solid fa-user"></i>`,
+  transfer:      `<i class="fa-solid fa-arrow-right-arrow-left"></i>`,
+  incoming:      `<i class="fa-solid fa-inbox"></i>`,
+  appearance:    `<i class="fa-solid fa-palette"></i>`,
+  notifications: `<i class="fa-solid fa-bell"></i>`,
+  data:          `<i class="fa-solid fa-trash-can"></i>`,
+  folder:        `<i class="fa-solid fa-folder-open"></i>`,
+  port:          `<i class="fa-solid fa-plug"></i>`,
 };
 
 export class SettingsView extends Component {
